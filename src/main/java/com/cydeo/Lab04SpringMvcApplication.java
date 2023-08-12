3499aea6fbf1fb243072f9a4fd18f12be9ef6b45
ziya <koltikin210@gmail.com>
2023-08-12 21:55:14 +0300

    Spring mvc lab task initial commit

---
+++ src/main/java/com/cydeo/Lab04SpringMvcApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab04SpringMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab04SpringMvcApplication.class, args);
+    }
+
+}
