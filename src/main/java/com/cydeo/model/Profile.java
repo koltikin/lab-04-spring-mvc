6d70307133986ba99f00065964b4057ca982acb7
ziya <koltikin210@gmail.com>
2023-08-12 22:53:39 +0300

    Spring mvc lab-list task is done

---
+++ src/main/java/com/cydeo/model/Profile.java
@@ -0,0 +1,18 @@
+package com.cydeo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class Profile {
+    private String email;
+    private String phoneNumber;
+    private String name;
+    private String surname;
+    private String userName;
+    private LocalDateTime createdDate;
+}
